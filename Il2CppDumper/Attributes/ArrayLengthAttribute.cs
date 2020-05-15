3ed99294263f2bac6bd92587a7a1f9a256ace97f
happybugme <121648331+happybugme@users.noreply.github.com>
2020-05-15 17:56:36 +0000

    chore(package): update lockfile package-lock.json

--- Il2CppDumper/Attributes/ArrayLengthAttribute.cs
+++ Il2CppDumper/Attributes/ArrayLengthAttribute.cs
@@ -1,1 +1,10 @@
-﻿using Syste
+﻿using System;
+
+namespace Il2CppDumper
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    class ArrayLengthAttribute : Attribute
+    {
+        public int Length { get; set; }
+    }
+}
