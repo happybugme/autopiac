480f3b9514de187e06fd7d760768a62e5be020d2
happybugme <121648331+happybugme@users.noreply.github.com>
2020-05-16 17:22:53 +0000

    API

--- Il2CppDumper/Attributes/VersionAttribute.cs
+++ Il2CppDumper/Attributes/VersionAttribute.cs
@@ -5,4 +5,7 @@ namespace Il2CppDumper
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
     class VersionAttribute : Attribute
     {
-        public double Min { get; s
+        public double Min { get; set; } = 0;
+        public double Max { get; set; } = 99;
+    }
+}
---
+++ Il2CppDumper/Config.cs
@@ -0,0 +1,6 @@
+﻿namespace Il2CppDumper
+{
+    public class Config
+    {
+        public bool DumpMethod { get; set; } = true;
+  
