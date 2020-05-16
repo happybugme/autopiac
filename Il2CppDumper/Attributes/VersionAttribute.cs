6f17f2d707f9a6a53aca7ff78ec96ee3b7cf65c3
happybugme <121648331+happybugme@users.noreply.github.com>
2020-05-16 11:13:40 +0000

    Procfile for heroku

--- Il2CppDumper/Attributes/VersionAttribute.cs
+++ Il2CppDumper/Attributes/VersionAttribute.cs
@@ -1,1 +1,8 @@
-﻿us
+﻿using System;
+
+namespace Il2CppDumper
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    class VersionAttribute : Attribute
+    {
+        public double Min { get; s
