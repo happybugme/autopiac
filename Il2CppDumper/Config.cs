8d6e40c1a59f1e82bb9d34a1a56246a188ae7fd6
happybugme <121648331+happybugme@users.noreply.github.com>
2020-05-27 05:39:28 +0000

    Fix linting issues

--- Il2CppDumper/Config.cs
+++ Il2CppDumper/Config.cs
@@ -6,4 +6,8 @@
         public bool DumpField { get; set; } = true;
         public bool DumpProperty { get; set; } = false;
         public bool DumpAttribute { get; set; } = false;
-        public bool DumpFieldOffset { get; set; }
+        public bool DumpFieldOffset { get; set; } = true;
+        public bool DumpMethodOffset { get; set; } = true;
+        public bool DumpTypeDefIndex { get; set; } = true;
+        public bool GenerateDummyDll { get; set; } = true;
+  
