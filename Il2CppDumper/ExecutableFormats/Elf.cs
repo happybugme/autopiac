85a4eb154d39e07705377076a0aee7d508ad264f
happybugme <121648331+happybugme@users.noreply.github.com>
2020-06-02 20:45:46 +0000

    fix #20

--- Il2CppDumper/ExecutableFormats/Elf.cs
+++ Il2CppDumper/ExecutableFormats/Elf.cs
@@ -4,4 +4,10 @@ using System.IO;
 using System.Linq;
 using static Il2CppDumper.ElfConstants;
 
-na
+namespace Il2CppDumper
+{
+    public sealed class Elf : ElfBase
+    {
+        private Elf32_Ehdr elfHeader;
+        private Elf32_Phdr[] programSegment;
+        private Elf32_Dyn[] dynamic
