1a34cf85713e3daba8070eafdb7f627114bdfe77
happybugme <121648331+happybugme@users.noreply.github.com>
2020-08-18 12:54:49 +0000

    chore(package): update ava to version 0.17.0

--- Il2CppDumper/ExecutableFormats/Elf.cs
+++ Il2CppDumper/ExecutableFormats/Elf.cs
@@ -180,4 +180,9 @@ namespace Il2CppDumper
                 Console.WriteLine("Detected Symbol !");
                 Console.WriteLine("CodeRegistration : {0:x}", codeRegistration);
                 Console.WriteLine("MetadataRegistration : {0:x}", metadataRegistration);
-                Init(co
+                Init(codeRegistration, metadataRegistration);
+                return true;
+            }
+            Console.WriteLine("ERROR: No symbol is detected");
+            return false;
+     
