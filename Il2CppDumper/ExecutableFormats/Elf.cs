f52ea184812ab72a81748878b928fcbd287f20e9
happybugme <121648331+happybugme@users.noreply.github.com>
2020-08-18 22:06:28 +0000

    changed email address

--- Il2CppDumper/ExecutableFormats/Elf.cs
+++ Il2CppDumper/ExecutableFormats/Elf.cs
@@ -210,4 +210,6 @@ namespace Il2CppDumper
                     var symoffset = ReadUInt32();
                     var bloom_size = ReadUInt32();
                     var bloom_shift = ReadUInt32();
-               
+                    var buckets_address = addr + 16 + (4 * bloom_size);
+                    var buckets = ReadClassArray<uint>(buckets_address, nbuckets);
+                    var last_symbol = buckets.Ma
