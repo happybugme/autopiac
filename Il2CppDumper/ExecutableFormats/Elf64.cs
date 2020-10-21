8d72860121825e99110289abafff71bb7e973635
happybugme <121648331+happybugme@users.noreply.github.com>
2020-10-21 15:43:00 +0000

    video added

--- Il2CppDumper/ExecutableFormats/Elf64.cs
+++ Il2CppDumper/ExecutableFormats/Elf64.cs
@@ -49,4 +49,6 @@ namespace Il2CppDumper
         {
             try
             {
-               
+                var names = new List<string>();
+                sectionTable = ReadClassArray<Elf64_Shdr>(elfHeader.e_shoff, elfHeader.e_shnum);
+                var shstrndx = sectionTable[elfHeader.e_shstrndx].sh
