d0354bf606790bfb1fdf978a156382a07251c1cd
happybugme <121648331+happybugme@users.noreply.github.com>
2020-11-17 15:29:02 +0000

    Updates to readme

--- Il2CppDumper/ExecutableFormats/Elf64.cs
+++ Il2CppDumper/ExecutableFormats/Elf64.cs
@@ -109,4 +109,7 @@ namespace Il2CppDumper
                 {
                     case "g_CodeRegistration":
                         codeRegistration = symbol.st_value;
-  
+                        break;
+                    case "g_MetadataRegistration":
+                        metadataRegistration = symbol.st_value;
+                        break;
