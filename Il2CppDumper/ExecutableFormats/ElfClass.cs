de1bb33f4fd8e118af7a4b49bbafbe97f9671898
happybugme <121648331+happybugme@users.noreply.github.com>
2021-03-18 12:39:44 +0000

    f2fs: fix incorrect parsing with option string

--- Il2CppDumper/ExecutableFormats/ElfClass.cs
+++ Il2CppDumper/ExecutableFormats/ElfClass.cs
@@ -5,4 +5,10 @@
         public uint ei_mag;
         public byte ei_class;
         public byte ei_data;
-        
+        public byte ei_version;
+        public byte ei_osabi;
+        public byte ei_abiversion;
+        [ArrayLength(Length = 7)]
+        public byte[] ei_pad;
+        public ushort e_type;
+    
