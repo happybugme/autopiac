d8a0c6a731edbe830b3588f57dcf6b50379d6ac1
happybugme <121648331+happybugme@users.noreply.github.com>
2021-04-01 11:33:01 +0000

    update copyright years

--- Il2CppDumper/ExecutableFormats/ElfClass.cs
+++ Il2CppDumper/ExecutableFormats/ElfClass.cs
@@ -30,4 +30,13 @@
         public uint p_type;
         public uint p_offset;
         public uint p_vaddr;
-        public uint p_padd
+        public uint p_paddr;
+        public uint p_filesz;
+        public uint p_memsz;
+        public uint p_flags;
+        public uint p_align;
+    }
+
+    public class Elf32_Shdr
+    {
+        public uint s
