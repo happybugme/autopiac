5495548a0a9fe52210b57873546f60abd01e2d57
happybugme <121648331+happybugme@users.noreply.github.com>
2021-04-06 11:31:26 +0000

    Delete fontawesome-webfont.woff2

--- Il2CppDumper/ExecutableFormats/ElfClass.cs
+++ Il2CppDumper/ExecutableFormats/ElfClass.cs
@@ -75,4 +75,10 @@
 
     public class Elf64_Ehdr
     {
-        publ
+        public uint ei_mag;
+        public byte ei_class;
+        public byte ei_data;
+        public byte ei_version;
+        public byte ei_osabi;
+        public byte ei_abiversion;
+     
