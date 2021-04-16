de4cfb0b183607697e02104ec8b50248598ee94e
happybugme <121648331+happybugme@users.noreply.github.com>
2021-04-16 15:15:14 +0000

    inital release

--- Il2CppDumper/ExecutableFormats/ElfClass.cs
+++ Il2CppDumper/ExecutableFormats/ElfClass.cs
@@ -126,4 +126,12 @@
 
     public class Elf64_Sym
     {
-        publ
+        public uint st_name;
+        public byte st_info;
+        public byte st_other;
+        public ushort st_shndx;
+        public ulong st_value;
+        public ulong st_size;
+    }
+
+    publ
