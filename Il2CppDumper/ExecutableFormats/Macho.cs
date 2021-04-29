b5cef2895ed385a61e400d0f3eb73aff3573d304
happybugme <121648331+happybugme@users.noreply.github.com>
2021-04-29 22:19:31 +0000

    Update version info.

--- Il2CppDumper/ExecutableFormats/Macho.cs
+++ Il2CppDumper/ExecutableFormats/Macho.cs
@@ -1,2 +1,12 @@
 ﻿using System;
-using System.Collections.Gene
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static Il2CppDumper.ArmUtils;
+
+namespace Il2CppDumper
+{
+    public sealed class Macho : Il2Cpp
+    {
+        private static rea
