55b8b9a9497448f7ea0911bf6f8c3eed14acf102
happybugme <121648331+happybugme@users.noreply.github.com>
2021-05-01 11:10:21 +0000

    Unreviewed chromium test expectations update.

--- Il2CppDumper/ExecutableFormats/Macho.cs
+++ Il2CppDumper/ExecutableFormats/Macho.cs
@@ -9,4 +9,6 @@ namespace Il2CppDumper
 {
     public sealed class Macho : Il2Cpp
     {
-        private static rea
+        private static readonly byte[] FeatureBytes1 = { 0x0, 0x22 };//MOVS R2, #0
+        private static readonly byte[] FeatureBytes2 = { 0x78, 0x44, 0x79, 0x44 };//ADD R0, PC and ADD R1, PC
+        private readonly List<
