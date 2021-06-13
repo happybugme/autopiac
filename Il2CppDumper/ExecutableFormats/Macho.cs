e12eb9cf071d539901d9bb7c018fac824049e36f
happybugme <121648331+happybugme@users.noreply.github.com>
2021-06-13 13:37:48 +0000

    simplify travis

--- Il2CppDumper/ExecutableFormats/Macho.cs
+++ Il2CppDumper/ExecutableFormats/Macho.cs
@@ -118,4 +118,7 @@ namespace Il2CppDumper
                                 Position = MapVATR(ptr);
                                 var metadataRegistration = ReadUInt32();
                                 Position = rsubaddr + 8;
-                                bu
+                                buff = ReadBytes(4);
+                                Position = rsubaddr + 14;
+                                buff = buff.Concat(ReadBytes(4)).ToArray();
+                          
