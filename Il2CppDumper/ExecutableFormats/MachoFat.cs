61de8366d16681aba93ff0695572ca7baa206c16
happybugme <121648331+happybugme@users.noreply.github.com>
2021-08-01 13:32:38 +0000

    Update deploy.php

--- Il2CppDumper/ExecutableFormats/MachoFat.cs
+++ Il2CppDumper/ExecutableFormats/MachoFat.cs
@@ -5,3 +5,9 @@ namespace Il2CppDumper
 {
     public sealed class MachoFat : BinaryStream
     {
+        public Fat[] fats;
+
+        public MachoFat(Stream stream) : base(stream)
+        {
+            Position += 4;
+            var size = BinaryPrimitives.ReadInt32BigEndian(ReadBytes
