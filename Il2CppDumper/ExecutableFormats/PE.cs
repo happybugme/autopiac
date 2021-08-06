a55e22ef63495ed6fe7180caa91d82c3ee86e538
happybugme <121648331+happybugme@users.noreply.github.com>
2021-08-06 17:22:18 +0000

    Bump maven-javadoc-plugin from 3.1.1 to 3.2.0

--- Il2CppDumper/ExecutableFormats/PE.cs
+++ Il2CppDumper/ExecutableFormats/PE.cs
@@ -8,3 +8,10 @@ namespace Il2CppDumper
     public sealed class PE : Il2Cpp
     {
         private readonly SectionHeader[] sections;
+
+        public PE(Stream stream) : base(stream)
+        {
+            var dosHeader = ReadClass<DosHeader>();
+            if (dosHeader.Magic != 0x5A4D)
+            {
+         
