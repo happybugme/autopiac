773389abfcd7fb5b28f3fc29f59037b97879d7f8
happybugme <121648331+happybugme@users.noreply.github.com>
2021-08-06 10:24:37 +0000

    Prepare 0.22.0 release

--- Il2CppDumper/ExecutableFormats/PE.cs
+++ Il2CppDumper/ExecutableFormats/PE.cs
@@ -1,1 +1,10 @@
-﻿using Sys
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Il2CppDumper
+{
+    public sealed class PE : Il2Cpp
+    {
+        private readonly SectionHeader[] sections;
