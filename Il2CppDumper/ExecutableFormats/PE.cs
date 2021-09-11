66b99991db32b44f85fc45996aaab6876e2c337b
happybugme <121648331+happybugme@users.noreply.github.com>
2021-09-11 08:47:56 +0000

    bump to 1.3.0

--- Il2CppDumper/ExecutableFormats/PE.cs
+++ Il2CppDumper/ExecutableFormats/PE.cs
@@ -83,4 +83,9 @@ namespace Il2CppDumper
         public override bool PlusSearch(int methodCount, int typeDefinitionsCount, int imageCount)
         {
             var sectionHelper = GetSectionHelper(methodCount, typeDefinitionsCount, imageCount);
-            var codeRegistration = sectionHelper.FindCod
+            var codeRegistration = sectionHelper.FindCodeRegistration();
+            var metadataRegistration = sectionHelper.FindMetadataRegistration();
+            return AutoPlusInit(codeRegistration, metadataRegistration);
+        }
+
+  
