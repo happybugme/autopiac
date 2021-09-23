28f2b9d13ac23a15ef1895cb8991701d8c1be175
happybugme <121648331+happybugme@users.noreply.github.com>
2021-09-23 18:26:36 +0000

    langpacks/english: update

--- Il2CppDumper/ExecutableFormats/WebAssembly.cs
+++ Il2CppDumper/ExecutableFormats/WebAssembly.cs
@@ -3,4 +3,10 @@ using System.IO;
 
 namespace Il2CppDumper
 {
-    public se
+    public sealed class WebAssembly : BinaryStream
+    {
+        private readonly DataSection[] dataSections;
+
+        public WebAssembly(Stream stream) : base(stream)
+        {
+            Is32Bit = tru
