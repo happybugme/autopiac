919ed5b8452a417c5f4d85adb584b77b30d915cc
happybugme <121648331+happybugme@users.noreply.github.com>
2021-11-07 15:48:29 +0000

    Fix StyleCI

--- Il2CppDumper/Extensions/HexExtensions.cs
+++ Il2CppDumper/Extensions/HexExtensions.cs
@@ -10,4 +10,9 @@ namespace Il2CppDumper
             return Convert.ToString(b, 2).PadLeft(8, '0');
         }
 
-        public static string HexToBin(this byte[
+        public static string HexToBin(this byte[] bytes)
+        {
+            var result = new StringBuilder(bytes.Length * 8);
+            foreach (var b in bytes)
+            {
+                result.Insert(0, b.HexToBin())
