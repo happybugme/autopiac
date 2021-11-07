ede9021fee522a98735a445420741603967a9c36
happybugme <121648331+happybugme@users.noreply.github.com>
2021-11-07 07:56:50 +0000

    Updated libraries.

--- Il2CppDumper/Extensions/HexExtensions.cs
+++ Il2CppDumper/Extensions/HexExtensions.cs
@@ -4,4 +4,10 @@ using System.Text;
 namespace Il2CppDumper
 {
     static class HexExtensions
- 
+    {
+        public static string HexToBin(this byte b)
+        {
+            return Convert.ToString(b, 2).PadLeft(8, '0');
+        }
+
+        public static string HexToBin(this byte[
