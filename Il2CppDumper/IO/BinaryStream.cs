f433a70bdb94f13be34c986f49d1241fd968452c
happybugme <121648331+happybugme@users.noreply.github.com>
2021-11-13 20:04:33 +0000

    Delete misc.xml

--- Il2CppDumper/IO/BinaryStream.cs
+++ Il2CppDumper/IO/BinaryStream.cs
@@ -49,3 +49,179 @@ namespace Il2CppDumper
         public uint ReadUInt32() => reader.ReadUInt32();
 
         public long ReadInt64() => reader.ReadInt64();
+
+        public ulong ReadUInt64() => reader.ReadUInt64();
+
+        public float ReadSingle() => reader.ReadSingle();
+
+        public double ReadDouble() => reader.ReadDouble();
+
+        public uint ReadCompressedUInt32() => reader.ReadCompressedUInt32();
+
+        public int ReadCompressedInt32() => reader.ReadCompressedInt32();
+
+        public uint ReadULeb128() => reader.ReadULeb128();
+
+        public void Write(bool value) => writer.Write(value);
+
+        public void Write(byte value) => writer.Write(value);
+
+        public void Write(sbyte value) => writer.Write(value);
+
+        public void Write(short value) => writer.Write(value);
+
+        public void Write(ushort value) => writer.Write(value);
+
+        public void Write(int value) => writer.Write(value);
+
+        public void Write(uint value) => writer.Write(value);
+
+        public void Write(long value) => writer.Write(value);
+
+        public void Write(ulong value) => writer.Write(value);
+
+        public void Write(float value) => writer.Write(value);
+
+        public void Write(double value) => writer.Write(value);
+
+        public ulong Position
+        {
+            get => (ulong)stream.Position;
+            set => stream.Position = (long)value;
+        }
+
+        public ulong Length => (ulong)stream.Length;
+
+        private object ReadPrimitive(Type type)
+        {
+            return type.Name switch
+            {
+                "Int32" => ReadInt32(),
+                "UInt32" => ReadUInt32(),
+                "Int16" => ReadInt16(),
+                "UInt16" => ReadUInt16(),
+                "Byte" => ReadByte(),
+                "Int64" => ReadIntPtr(),
+                "UInt64" => ReadUIntPtr(),
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        public T ReadClass<T>(ulong addr) where T : new()
+        {
+            Position = addr;
+            return ReadClass<T>();
+        }
+
+        public T ReadClass<T>() where T : new()
+        {
+            var type = typeof(T);
+            if (type.IsPrimitive)
+            {
+                return (T)ReadPrimitive(type);
+            }
+            else
+            {
+                var t = new T();
+                foreach (var i in t.GetType().GetFields())
+                {
+                    if (!attributeCache.TryGetValue(i, out var versionAttributes))
+                    {
+                        if (Attribute.IsDefined(i, typeof(VersionAttribute)))
+                        {
+                            versionAttributes = i.GetCustomAttributes<VersionAttribute>().ToArray();
+                            attributeCache.Add(i, versionAttributes);
+                        }
+                    }
+                    if (versionAttributes?.Length > 0)
+                    {
+                        var read = false;
+                        foreach (var versionAttribute in versionAttributes)
+                        {
+                            if (Version >= versionAttribute.Min && Version <= versionAttribute.Max)
+                            {
+                                read = true;
+                                break;
+                            }
+                        }
+                        if (!read)
+                        {
+                            continue;
+                        }
+                    }
+                    var fieldType = i.FieldType;
+                    if (fieldType.IsPrimitive)
+                    {
+                        i.SetValue(t, ReadPrimitive(fieldType));
+                    }
+                    else if (fieldType.IsEnum)
+                    {
+                        var e = fieldType.GetField("value__").FieldType;
+                        i.SetValue(t, ReadPrimitive(e));
+                    }
+                    else if (fieldType.IsArray)
+                    {
+                        var arrayLengthAttribute = i.GetCustomAttribute<ArrayLengthAttribute>();
+                        if (!genericMethodCache.TryGetValue(fieldType, out var methodInfo))
+                        {
+                            methodInfo = readClassArray.MakeGenericMethod(fieldType.GetElementType());
+                            genericMethodCache.Add(fieldType, methodInfo);
+                        }
+                        i.SetValue(t, methodInfo.Invoke(this, new object[] { arrayLengthAttribute.Length }));
+                    }
+                    else
+                    {
+                        if (!genericMethodCache.TryGetValue(fieldType, out var methodInfo))
+                        {
+                            methodInfo = readClass.MakeGenericMethod(fieldType);
+                            genericMethodCache.Add(fieldType, methodInfo);
+                        }
+                        i.SetValue(t, methodInfo.Invoke(this, null));
+                    }
+                }
+                return t;
+            }
+        }
+
+        public T[] ReadClassArray<T>(long count) where T : new()
+        {
+            var t = new T[count];
+            for (var i = 0; i < count; i++)
+            {
+                t[i] = ReadClass<T>();
+            }
+            return t;
+        }
+
+        public T[] ReadClassArray<T>(ulong addr, ulong count) where T : new()
+        {
+            return ReadClassArray<T>(addr, (long)count);
+        }
+
+        public T[] ReadClassArray<T>(ulong addr, long count) where T : new()
+        {
+            Position = addr;
+            return ReadClassArray<T>(count);
+        }
+
+        public string ReadStringToNull(ulong addr)
+        {
+            Position = addr;
+            var bytes = new List<byte>();
+            byte b;
+            while ((b = ReadByte()) != 0)
+                bytes.Add(b);
+            return Encoding.UTF8.GetString(bytes.ToArray());
+        }
+
+        public long ReadIntPtr()
+        {
+            return Is32Bit ? ReadInt32() : ReadInt64();
+        }
+
+        public virtual ulong ReadUIntPtr()
+        {
+            return Is32Bit ? ReadUInt32() : ReadUInt64();
+        }
+
+        public ulong PointerSize
