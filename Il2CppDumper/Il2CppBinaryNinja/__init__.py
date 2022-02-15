184acab8315a059476e6a8c85947c0c228b87567
happybugme <121648331+happybugme@users.noreply.github.com>
2022-02-15 18:35:42 +0000

    update po

--- Il2CppDumper/Il2CppBinaryNinja/__init__.py
+++ Il2CppDumper/Il2CppBinaryNinja/__init__.py
@@ -1,2 +1,9 @@
 from binaryninja import *
-from os.path import exist
+from os.path import exists
+
+def get_addr(bv: BinaryView, addr: int):
+    imageBase = bv.start
+    return imageBase + addr
+
+class Il2CppProcessTask(BackgroundTaskThread):
+    def __init__(self, bv: BinaryView, script_path: s
