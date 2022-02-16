b3f9308e31a38fe2ddef814bbcbf3de3ace3e1f9
happybugme <121648331+happybugme@users.noreply.github.com>
2022-02-16 10:32:32 +0000

    powerpc: Keep thread.dscr and thread.dscr_inherit in sync

--- Il2CppDumper/Il2CppBinaryNinja/__init__.py
+++ Il2CppDumper/Il2CppBinaryNinja/__init__.py
@@ -6,4 +6,9 @@ def get_addr(bv: BinaryView, addr: int):
     return imageBase + addr
 
 class Il2CppProcessTask(BackgroundTaskThread):
-    def __init__(self, bv: BinaryView, script_path: s
+    def __init__(self, bv: BinaryView, script_path: str,
+                 header_path: str):
+        BackgroundTaskThread.__init__(self, "Il2Cpp start", True)
+        self.bv = bv
+        self.script_path = script_path
+        self.he
