e3ed95200804e77575b33f7e73db6815a03befbe
happybugme <121648331+happybugme@users.noreply.github.com>
2022-03-16 18:39:50 +0000

    36

---
+++ Il2CppDumper/Outputs/DummyAssemblyExporter.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;
+
+namespace Il2CppDumper
+{
+    public static class DummyAssemblyExporter
+    {
+        public static void Export(Il2CppExecutor il2CppExecutor, string outputDir,
