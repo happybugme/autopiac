ac60762a1b6207dcc8110c6899361d2dafddc975
happybugme <121648331+happybugme@users.noreply.github.com>
2022-03-17 14:19:24 +0000

    Version 1.1r57

--- Il2CppDumper/Outputs/DummyAssemblyExporter.cs
+++ Il2CppDumper/Outputs/DummyAssemblyExporter.cs
@@ -4,4 +4,9 @@ namespace Il2CppDumper
 {
     public static class DummyAssemblyExporter
     {
-        public static void Export(Il2CppExecutor il2CppExecutor, string outputDir,
+        public static void Export(Il2CppExecutor il2CppExecutor, string outputDir, bool addToken)
+        {
+            Directory.SetCurrentDirectory(outputDir);
+            if (Directory.Exists("DummyDll"))
+                Directory.Delete("DummyDll", true);
+   
