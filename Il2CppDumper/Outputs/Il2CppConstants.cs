48fa6ac1684782712478c8b75043bbe25e5bdb2b
happybugme <121648331+happybugme@users.noreply.github.com>
2022-03-31 21:45:09 +0000

    Bump version to 1.4.3

--- Il2CppDumper/Outputs/Il2CppConstants.cs
+++ Il2CppDumper/Outputs/Il2CppConstants.cs
@@ -5,4 +5,6 @@
         /*
          * Field Attributes (21.1.5).
          */
-   
+        public const int FIELD_ATTRIBUTE_FIELD_ACCESS_MASK = 0x0007;
+        public const int FIELD_ATTRIBUTE_COMPILER_CONTROLLED = 0x0000;
+        public const int FIELD_ATTRIBUTE_PRIVATE = 0x0
