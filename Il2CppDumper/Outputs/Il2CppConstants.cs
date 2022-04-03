af5085947344d2f668333c6ddfa3e6b6d1317242
happybugme <121648331+happybugme@users.noreply.github.com>
2022-04-03 07:06:17 +0000

    Removed completed TODO

--- Il2CppDumper/Outputs/Il2CppConstants.cs
+++ Il2CppDumper/Outputs/Il2CppConstants.cs
@@ -7,4 +7,8 @@
          */
         public const int FIELD_ATTRIBUTE_FIELD_ACCESS_MASK = 0x0007;
         public const int FIELD_ATTRIBUTE_COMPILER_CONTROLLED = 0x0000;
-        public const int FIELD_ATTRIBUTE_PRIVATE = 0x0
+        public const int FIELD_ATTRIBUTE_PRIVATE = 0x0001;
+        public const int FIELD_ATTRIBUTE_FAM_AND_ASSEM = 0x0002;
+        public const int FIELD_ATTRIBUTE_ASSEMBLY = 0x0003;
+        public const int FIELD_ATTRIBUTE_FAMILY = 0x0004;
+ 
