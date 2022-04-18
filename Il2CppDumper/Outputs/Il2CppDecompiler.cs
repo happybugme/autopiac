9af5c33d5af7c2d2e991f71fd36ac70cfcc88f18
happybugme <121648331+happybugme@users.noreply.github.com>
2022-04-18 14:01:55 +0000

    docstrings

--- Il2CppDumper/Outputs/Il2CppDecompiler.cs
+++ Il2CppDumper/Outputs/Il2CppDecompiler.cs
@@ -45,3 +45,185 @@ namespace Il2CppDumper
                         var extends = new List<string>();
                         if (typeDef.parentIndex >= 0)
                         {
+                            var parent = il2Cpp.types[typeDef.parentIndex];
+                            var parentName = executor.GetTypeName(parent, false, false);
+                            if (!typeDef.IsValueType && !typeDef.IsEnum && parentName != "object")
+                            {
+                                extends.Add(parentName);
+                            }
+                        }
+                        if (typeDef.interfaces_count > 0)
+                        {
+                            for (int i = 0; i < typeDef.interfaces_count; i++)
+                            {
+                                var @interface = il2Cpp.types[metadata.interfaceIndices[typeDef.interfacesStart + i]];
+                                extends.Add(executor.GetTypeName(@interface, false, false));
+                            }
+                        }
+                        writer.Write($"\n// Namespace: {metadata.GetStringFromIndex(typeDef.namespaceIndex)}\n");
+                        if (config.DumpAttribute)
+                        {
+                            writer.Write(GetCustomAttribute(imageDef, typeDef.customAttributeIndex, typeDef.token));
+                        }
+                        if (config.DumpAttribute && (typeDef.flags & TYPE_ATTRIBUTE_SERIALIZABLE) != 0)
+                            writer.Write("[Serializable]\n");
+                        var visibility = typeDef.flags & TYPE_ATTRIBUTE_VISIBILITY_MASK;
+                        switch (visibility)
+                        {
+                            case TYPE_ATTRIBUTE_PUBLIC:
+                            case TYPE_ATTRIBUTE_NESTED_PUBLIC:
+                                writer.Write("public ");
+                                break;
+                            case TYPE_ATTRIBUTE_NOT_PUBLIC:
+                            case TYPE_ATTRIBUTE_NESTED_FAM_AND_ASSEM:
+                            case TYPE_ATTRIBUTE_NESTED_ASSEMBLY:
+                                writer.Write("internal ");
+                                break;
+                            case TYPE_ATTRIBUTE_NESTED_PRIVATE:
+                                writer.Write("private ");
+                                break;
+                            case TYPE_ATTRIBUTE_NESTED_FAMILY:
+                                writer.Write("protected ");
+                                break;
+                            case TYPE_ATTRIBUTE_NESTED_FAM_OR_ASSEM:
+                                writer.Write("protected internal ");
+                                break;
+                        }
+                        if ((typeDef.flags & TYPE_ATTRIBUTE_ABSTRACT) != 0 && (typeDef.flags & TYPE_ATTRIBUTE_SEALED) != 0)
+                            writer.Write("static ");
+                        else if ((typeDef.flags & TYPE_ATTRIBUTE_INTERFACE) == 0 && (typeDef.flags & TYPE_ATTRIBUTE_ABSTRACT) != 0)
+                            writer.Write("abstract ");
+                        else if (!typeDef.IsValueType && !typeDef.IsEnum && (typeDef.flags & TYPE_ATTRIBUTE_SEALED) != 0)
+                            writer.Write("sealed ");
+                        if ((typeDef.flags & TYPE_ATTRIBUTE_INTERFACE) != 0)
+                            writer.Write("interface ");
+                        else if (typeDef.IsEnum)
+                            writer.Write("enum ");
+                        else if (typeDef.IsValueType)
+                            writer.Write("struct ");
+                        else
+                            writer.Write("class ");
+                        var typeName = executor.GetTypeDefName(typeDef, false, true);
+                        writer.Write($"{typeName}");
+                        if (extends.Count > 0)
+                            writer.Write($" : {string.Join(", ", extends)}");
+                        if (config.DumpTypeDefIndex)
+                            writer.Write($" // TypeDefIndex: {typeDefIndex}\n{{");
+                        else
+                            writer.Write("\n{");
+                        //dump field
+                        if (config.DumpField && typeDef.field_count > 0)
+                        {
+                            writer.Write("\n\t// Fields\n");
+                            var fieldEnd = typeDef.fieldStart + typeDef.field_count;
+                            for (var i = typeDef.fieldStart; i < fieldEnd; ++i)
+                            {
+                                var fieldDef = metadata.fieldDefs[i];
+                                var fieldType = il2Cpp.types[fieldDef.typeIndex];
+                                var isStatic = false;
+                                var isConst = false;
+                                if (config.DumpAttribute)
+                                {
+                                    writer.Write(GetCustomAttribute(imageDef, fieldDef.customAttributeIndex, fieldDef.token, "\t"));
+                                }
+                                writer.Write("\t");
+                                var access = fieldType.attrs & FIELD_ATTRIBUTE_FIELD_ACCESS_MASK;
+                                switch (access)
+                                {
+                                    case FIELD_ATTRIBUTE_PRIVATE:
+                                        writer.Write("private ");
+                                        break;
+                                    case FIELD_ATTRIBUTE_PUBLIC:
+                                        writer.Write("public ");
+                                        break;
+                                    case FIELD_ATTRIBUTE_FAMILY:
+                                        writer.Write("protected ");
+                                        break;
+                                    case FIELD_ATTRIBUTE_ASSEMBLY:
+                                    case FIELD_ATTRIBUTE_FAM_AND_ASSEM:
+                                        writer.Write("internal ");
+                                        break;
+                                    case FIELD_ATTRIBUTE_FAM_OR_ASSEM:
+                                        writer.Write("protected internal ");
+                                        break;
+                                }
+                                if ((fieldType.attrs & FIELD_ATTRIBUTE_LITERAL) != 0)
+                                {
+                                    isConst = true;
+                                    writer.Write("const ");
+                                }
+                                else
+                                {
+                                    if ((fieldType.attrs & FIELD_ATTRIBUTE_STATIC) != 0)
+                                    {
+                                        isStatic = true;
+                                        writer.Write("static ");
+                                    }
+                                    if ((fieldType.attrs & FIELD_ATTRIBUTE_INIT_ONLY) != 0)
+                                    {
+                                        writer.Write("readonly ");
+                                    }
+                                }
+                                writer.Write($"{executor.GetTypeName(fieldType, false, false)} {metadata.GetStringFromIndex(fieldDef.nameIndex)}");
+                                if (metadata.GetFieldDefaultValueFromIndex(i, out var fieldDefaultValue) && fieldDefaultValue.dataIndex != -1)
+                                {
+                                    if (executor.TryGetDefaultValue(fieldDefaultValue.typeIndex, fieldDefaultValue.dataIndex, out var value))
+                                    {
+                                        writer.Write($" = ");
+                                        if (value is string str)
+                                        {
+                                            writer.Write($"\"{str.ToEscapedString()}\"");
+                                        }
+                                        else if (value is char c)
+                                        {
+                                            var v = (int)c;
+                                            writer.Write($"'\\x{v:x}'");
+                                        }
+                                        else if (value != null)
+                                        {
+                                            writer.Write($"{value}");
+                                        }
+                                        else
+                                        {
+                                            writer.Write("null");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        writer.Write($" /*Metadata offset 0x{value:X}*/");
+                                    }
+                                }
+                                if (config.DumpFieldOffset && !isConst)
+                                    writer.Write("; // 0x{0:X}\n", il2Cpp.GetFieldOffsetFromIndex(typeDefIndex, i - typeDef.fieldStart, i, typeDef.IsValueType, isStatic));
+                                else
+                                    writer.Write(";\n");
+                            }
+                        }
+                        //dump property
+                        if (config.DumpProperty && typeDef.property_count > 0)
+                        {
+                            writer.Write("\n\t// Properties\n");
+                            var propertyEnd = typeDef.propertyStart + typeDef.property_count;
+                            for (var i = typeDef.propertyStart; i < propertyEnd; ++i)
+                            {
+                                var propertyDef = metadata.propertyDefs[i];
+                                if (config.DumpAttribute)
+                                {
+                                    writer.Write(GetCustomAttribute(imageDef, propertyDef.customAttributeIndex, propertyDef.token, "\t"));
+                                }
+                                writer.Write("\t");
+                                if (propertyDef.get >= 0)
+                                {
+                                    var methodDef = metadata.methodDefs[typeDef.methodStart + propertyDef.get];
+                                    writer.Write(GetModifiers(methodDef));
+                                    var propertyType = il2Cpp.types[methodDef.returnType];
+                                    writer.Write($"{executor.GetTypeName(propertyType, false, false)} {metadata.GetStringFromIndex(propertyDef.nameIndex)} {{ ");
+                                }
+                                else if (propertyDef.set >= 0)
+                                {
+                                    var methodDef = metadata.methodDefs[typeDef.methodStart + propertyDef.set];
+                                    writer.Write(GetModifiers(methodDef));
+                                    var parameterDef = metadata.parameterDefs[methodDef.parameterStart];
+                                    var propertyType = il2Cpp.types[parameterDef.typeIndex];
+                                    writer.Write($"{executor.GetTypeName(propertyType, false, false)} {metadata.GetStringFromIndex(propertyDef.nameIndex)} {{ ");
+                                }
