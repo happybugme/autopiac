2213edc8a16fda6433ae9eca7f813cb9eb079e06
happybugme <121648331+happybugme@users.noreply.github.com>
2022-05-01 06:50:46 +0000

    cpufreq: Memory leak with sysfs when stats table are freed

--- Il2CppDumper/Program.cs
+++ Il2CppDumper/Program.cs
@@ -1,3 +1,14 @@
 ﻿using System;
 using System.IO;
-using System
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace Il2CppDumper
+{
+    class Program
+    {
+        private static Config config;
+
+        [STAThread]
+        static void Main(string[
