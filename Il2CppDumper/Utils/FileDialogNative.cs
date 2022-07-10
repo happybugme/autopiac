ebb8badd949400edab9a28722e2acab139c68501
happybugme <121648331+happybugme@users.noreply.github.com>
2022-07-10 21:07:16 +0000

    auto update

--- Il2CppDumper/Utils/FileDialogNative.cs
+++ Il2CppDumper/Utils/FileDialogNative.cs
@@ -16,4 +16,6 @@ namespace Il2CppDumper
         {
             private IIDGuid() { } // Avoid FxCop violation AvoidUninstantiatedInternalClasses
             // IID GUID strings for relevant COM interfaces
-            internal const st
+            internal const string IModalWindow = "b4db1657-70d7-485e-8e3e-6fcb5a5c1802";
+            internal const string IFileDialog = "42f85136-db7e-439c-85f1-e4075d135fc8";
+            internal const str
