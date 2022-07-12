38825435e74091af1144cd023abeb388e397218d
happybugme <121648331+happybugme@users.noreply.github.com>
2022-07-12 14:45:59 +0000

    Update indentation to use tabs

--- Il2CppDumper/Utils/FileDialogNative.cs
+++ Il2CppDumper/Utils/FileDialogNative.cs
@@ -30,4 +30,11 @@ namespace Il2CppDumper
             private CLSIDGuid() { } // Avoid FxCop violation AvoidUninstantiatedInternalClasses
             internal const string FileOpenDialog = "DC1C5A9C-E88A-4dde-A5A1-60F82A20AEF7";
             internal const string FileSaveDialog = "C0B4E2F3-BA21-4773-8DBA-335EC946EB8B";
-   
+        }
+
+        [ComImport()]
+        [Guid(IIDGuid.IFileDialog)]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IFileDialog
+        {
+            [Pres
