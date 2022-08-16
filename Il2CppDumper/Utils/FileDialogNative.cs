d1aa65f3e0c8dde7bce807991698b2c80dec00f7
happybugme <121648331+happybugme@users.noreply.github.com>
2022-08-16 21:54:49 +0000

    Pontoon: Update Welsh (cy) localization of Firefox Screenshots

--- Il2CppDumper/Utils/FileDialogNative.cs
+++ Il2CppDumper/Utils/FileDialogNative.cs
@@ -184,4 +184,10 @@ namespace Il2CppDumper
         }
 
         internal enum FDE_OVERWRITE_RESPONSE
-     
+        {
+            FDEOR_DEFAULT = 0x00000000,
+            FDEOR_ACCEPT = 0x00000001,
+            FDEOR_REFUSE = 0x00000002
+        }
+    }
+}
---
+++ Il2CppDumper/Utils/Il2CppExecutor.cs
@@ -0,0 +1,47 @@
+
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Il2CppDumper
+{
+    public class Il2CppExecutor
+    {
+        public Metadata metadata;
+        public Il2Cpp il2Cpp;
+        private static readonly Dictionary<int, string> TypeString = new()
+        {
+            {1,"void"},
+            {2,"bool"},
+            {3,"char"},
+            {4,"sbyte"},
+            {5,"byte"},
+            {6,"short"},
+            {7,"ushort"},
+            {8,"int"},
+            {9,"uint"},
+            {10,"long"},
+            {11,"ulong"},
+            {12,"float"},
+            {13,"double"},
+            {14,"string"},
+            {22,"TypedReference"},
+            {24,"IntPtr"},
+            {25,"UIntPtr"},
+            {28,"object"},
+        };
+        public ulong[] customAttributeGenerators;
+
+        public Il2CppExecutor(Metadata metadata, Il2Cpp il2Cpp)
+        {
+            this.metadata = metadata;
+            this.il2Cpp = il2Cpp;
+
+            if (il2Cpp.Version >= 27 && il2Cpp.Version < 29)
+            {
+                customAttributeGenerators = new ulong[metadata.imageDefs.Sum(x => x.customAttributeCount)];
+                foreach (var imageDef in metadata.imageDefs)
+                {
+                    var imageDefName = metadata.GetStringFromIndex(imageDef.nameIndex);
