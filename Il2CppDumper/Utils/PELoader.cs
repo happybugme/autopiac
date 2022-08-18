e42bf4ba387d9e615cc03ee73075e815bcf5a58a
happybugme <121648331+happybugme@users.noreply.github.com>
2022-08-18 07:24:18 +0000

    updated dist

--- Il2CppDumper/Utils/PELoader.cs
+++ Il2CppDumper/Utils/PELoader.cs
@@ -29,3 +29,46 @@ namespace Il2CppDumper
             var fileHeader = reader.ReadClass<FileHeader>();
             if (fileHeader.Machine == 0x14c && Environment.Is64BitProcess) //64bit process can't load 32bit dll
             {
+                throw new InvalidOperationException("The file is a 32-bit file, please try to load it with Il2CppDumper-x86.exe");
+            }
+            if (fileHeader.Machine == 0x8664 && !Environment.Is64BitProcess) //32bit process can't load 64bit dll
+            {
+                throw new InvalidOperationException("The file is a 64-bit file, please try to load it with Il2CppDumper.exe");
+            }
+            var pos = reader.Position;
+            reader.Position = pos + fileHeader.SizeOfOptionalHeader;
+            var sections = reader.ReadClassArray<SectionHeader>(fileHeader.NumberOfSections);
+            var last = sections[^1];
+            var size = last.VirtualAddress + last.VirtualSize;
+            var peBuff = new byte[size];
+            var handle = LoadLibrary(fileName);
+            if (handle == IntPtr.Zero)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+            foreach (var section in sections)
+            {
+                switch (section.Characteristics)
+                {
+                    case 0x60000020:
+                    case 0x40000040:
+                    case 0xC0000040:
+                        Marshal.Copy(new IntPtr(handle.ToInt64() + section.VirtualAddress), peBuff, (int)section.VirtualAddress, (int)section.VirtualSize);
+                        break;
+                }
+            }
+            var peMemory = new MemoryStream(peBuff);
+            var writer = new BinaryWriter(peMemory, Encoding.UTF8, true);
+            var headerSize = reader.Position;
+            reader.Position = 0;
+            var buff2 = reader.ReadBytes((int)headerSize);
+            writer.Write(buff2);
+            writer.Flush();
+            writer.Close();
+            peMemory.Position = 0;
+            var pe = new PE(peMemory);
+            pe.LoadFromMemory((ulong)handle.ToInt64());
+            return pe;
+        }
+    }
+}
---
+++ Il2CppDumper/Utils/SectionHelper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Il2CppDumper
+{
+    public class SectionHelper
+  
