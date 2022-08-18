1d4bcef8c14100d7d20c70c2ce7a7e362ee7b48b
happybugme <121648331+happybugme@users.noreply.github.com>
2022-08-18 13:00:16 +0000

    target: Fix R_HOLDER bit usage for AllRegistrants

--- Il2CppDumper/Utils/SectionHelper.cs
+++ Il2CppDumper/Utils/SectionHelper.cs
@@ -5,4 +5,9 @@ using System.Linq;
 namespace Il2CppDumper
 {
     public class SectionHelper
-  
+    {
+        private List<SearchSection> exec;
+        private List<SearchSection> data;
+        private List<SearchSection> bss;
+        private readonly Il2Cpp il2Cpp;
+        private readonly int methodCount;
