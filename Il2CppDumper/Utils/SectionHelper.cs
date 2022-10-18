e7e631fafcda5d65c415b6314e85508efb5d12ac
happybugme <121648331+happybugme@users.noreply.github.com>
2022-10-18 11:29:55 +0000

    vuplus: update bsp

--- Il2CppDumper/Utils/SectionHelper.cs
+++ Il2CppDumper/Utils/SectionHelper.cs
@@ -380,4 +380,7 @@ namespace Il2CppDumper
                                         il2Cpp.Position = il2Cpp.MapVATR(refva3 - il2Cpp.PointerSize);
                                         if (il2Cpp.ReadIntPtr() == imageCount)
                                         {
-                        
+                                            if (il2Cpp.Version >= 29)
+                                            {
+                                                return refva3 - il2Cpp.PointerSize * 14;
+            
