48b9c37b906eb6cfe9a87e6a948e6a0927df4449
happybugme <121648331+happybugme@users.noreply.github.com>
2022-10-29 19:55:54 +0000

    spell fix

---
+++ Il2CppDumper/ghidra.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import json
+
+processFields = [
+	"ScriptMethod",
+	"ScriptString",
+	"ScriptMetadata",
+	"ScriptMetadataMethod",
+	"Addresses",
+]
+
+functionManager = cu
