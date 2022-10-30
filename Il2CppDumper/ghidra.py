bf0433b33b37cf058f36c7e7025acdaa7ef8b936
happybugme <121648331+happybugme@users.noreply.github.com>
2022-10-30 11:08:30 +0000

    merge from branch

--- Il2CppDumper/ghidra.py
+++ Il2CppDumper/ghidra.py
@@ -14,4 +14,13 @@ baseAddress = currentProgram.getImageBase()
 USER_DEFINED = ghidra.program.model.symbol.SourceType.USER_DEFINED
 
 def get_addr(addr):
-	return baseAddress.add
+	return baseAddress.add(addr)
+
+def set_name(addr, name):
+	name = name.replace(' ', '-')
+	createLabel(addr, name, True, USER_DEFINED)
+
+def make_function(start):
+	func = getFunctionAt(start)
+	if func is None:
+		createFun
