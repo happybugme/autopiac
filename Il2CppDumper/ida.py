cd8cd64d5b8078b1e527d9116d1020ddecf37ade
happybugme <121648331+happybugme@users.noreply.github.com>
2023-01-01 21:44:44 +0000

    media queries

--- Il2CppDumper/ida.py
+++ Il2CppDumper/ida.py
@@ -4,4 +4,15 @@ import json
 processFields = [
 	"ScriptMethod",
 	"ScriptString",
-	"ScriptMeta
+	"ScriptMetadata",
+	"ScriptMetadataMethod",
+	"Addresses",
+]
+
+imageBase = idaapi.get_imagebase()
+
+def get_addr(addr):
+	return imageBase + addr
+
+def set_name(addr, name):
+	ret = idc.set_name(addr, na
