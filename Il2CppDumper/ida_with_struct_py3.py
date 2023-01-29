c5a3541d28c39630a00838e2dbee068a96ef79ed
happybugme <121648331+happybugme@users.noreply.github.com>
2023-01-29 21:20:31 +0000

    Revert "Initial commit"

--- Il2CppDumper/ida_with_struct_py3.py
+++ Il2CppDumper/ida_with_struct_py3.py
@@ -15,4 +15,10 @@ def get_addr(addr):
 	return imageBase + addr
 
 def set_name(addr, name):
-	ret = idc.set_name(
+	ret = idc.set_name(addr, name, SN_NOWARN | SN_NOCHECK)
+	if ret == 0:
+		new_name = name + '_' + str(addr)
+		ret = idc.set_name(addr, new_name, SN_NOWARN | SN_NOCHECK)
+
+def make_function(start, end):
+	next_func 
