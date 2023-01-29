64be4da2b0731b2b7705267cf044321eb1b4ff8f
happybugme <121648331+happybugme@users.noreply.github.com>
2023-01-29 16:29:31 +0000

    Updated changes

--- Il2CppDumper/ida_with_struct_py3.py
+++ Il2CppDumper/ida_with_struct_py3.py
@@ -3,4 +3,16 @@ import json
 
 processFields = [
 	"ScriptMethod",
-	"Scr
+	"ScriptString",
+	"ScriptMetadata",
+	"ScriptMetadataMethod",
+	"Addresses",
+]
+
+imageBase = idaapi.get_imagebase()
+
+def get_addr(addr):
+	return imageBase + addr
+
+def set_name(addr, name):
+	ret = idc.set_name(
