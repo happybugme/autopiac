ed906ed5f43e76345e0f8e762306bc140c4db2ab
happybugme <121648331+happybugme@users.noreply.github.com>
2023-02-18 22:55:31 +0000

    edit .gitignore

--- Il2CppDumper/il2cpp_header_to_ghidra.py
+++ Il2CppDumper/il2cpp_header_to_ghidra.py
@@ -7,4 +7,8 @@ header = "typedef unsigned __int8 uint8_t;\n" \
          "typedef __int8 int8_t;\n" \
          "typedef __int16 int16_t;\n" \
          "typedef __int32 int32_t;\n" \
-         "typedef __int64 int6
+         "typedef __int64 int64_t;\n" \
+         "typedef __int64 intptr_t;\n" \
+         "typedef __int64 uintptr_t;\n" \
+         "typedef unsigned __int64 size_t;\n" \
+         "typedef _Bool boo
