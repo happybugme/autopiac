4a0114c96c669a133998bafba962f1837c74cf6e
happybugme <121648331+happybugme@users.noreply.github.com>
2023-02-18 05:50:08 +0000

    #merge

--- Il2CppDumper/il2cpp_header_to_ghidra.py
+++ Il2CppDumper/il2cpp_header_to_ghidra.py
@@ -3,4 +3,8 @@
 header = "typedef unsigned __int8 uint8_t;\n" \
          "typedef unsigned __int16 uint16_t;\n" \
          "typedef unsigned __int32 uint32_t;\n" \
-         "ty
+         "typedef unsigned __int64 uint64_t;\n" \
+         "typedef __int8 int8_t;\n" \
+         "typedef __int16 int16_t;\n" \
+         "typedef __int32 int32_t;\n" \
+         "typedef __int64 int6
